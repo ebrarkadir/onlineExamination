668e11a4b6dbad7a34a5edcafc453d53106cefbb
Ebrar Kadir Çetin <cetinebrarkadir@gmail.com>
2023-08-16 13:39:11 +0300

    dataAccess0.1

---
+++ OnlineExamination.DataAccess/Exams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExamination.DataAccess
+{
+    public class Exams
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }  
+        public DateTime StartDate { get; set; }
+        public int Time { get; set; }
+        public int GroupsId { get; set; }
+        public Groups Groups { get; set; }
+        public ICollection<ExamResaults> ExamResaults { get; set; } = new List<ExamResaults>();
+        public ICollection<QnAs> QnAs { get; set; }
+    }
+}
--- OnlineExamination.DataAccess/Users.cs
+++ OnlineExamination.DataAccess/Users.cs
@@ -7,5 +7,6 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public int Role { get; set; }
+        public ICollection<Groups> Groups { get; set; } = new HashSet<Groups>();
     }
 }
