85bcb0b555c49e10d81916025b085b8aec183259
Ebrar Kadir Çetin <cetinebrarkadir@gmail.com>
2023-08-26 20:05:44 +0300

    QnAsController_Added

--- WebApplication2/Controllers/QnAsController.cs
+++ WebApplication2/Controllers/QnAsController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExamination.BLL.Services.Abstract;
+using OnlineExamination.ViewModels;
 
 namespace OnlineExamination.Web.Controllers
 {
     public class QnAsController : Controller
     {
-        public IActionResult Index()
+        private readonly IExamService _examService;
+        private readonly IQnAService _qnAService;
+
+        public QnAsController(IExamService examService, IQnAService qnAService)
         {
-            return View();
+            _examService = examService;
+            _qnAService = qnAService;
+        }
+
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+        {
+            return View(_qnAService.GetAll(pageNumber, pageSize));
+        }
+        public IActionResult Create()
+        {
+            var model = new QnAsViewModel();
+            model.ExamList = _examService.GetAllExams();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(QnAsViewModel qnAsViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _qnAService.AddSync(qnAsViewModel);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(qnAsViewModel);
         }
     }
 }
